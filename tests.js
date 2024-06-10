b65162e4bd7586a922e09c90f8456d4a863394d5
1sarso <125144275+1sarso@users.noreply.github.com>
2024-06-10 09:24:24 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,61 +1,61 @@
-// Example test case
-test("Example", function (assert) {
-    assert.propEqual(convertRomanToInteger("I"), { value: 1, message: '', result: true }, "TC-1");
+const { convertRomanToInteger, convertIntegerToRoman } = require('./romans.js');
+
+test('Example', () => {
+  expect(convertRomanToInteger('I')).toEqual({ value: 1, message: '', result: true });
 });
 
-// Test cases from task #4
-test("TC-1", function (assert) {
-    assert.propEqual(convertIntegerToRoman(1), { value: 'I', message: '', result: true }, "TC-1");
+test('TC-1', () => {
+  expect(convertIntegerToRoman(1)).toEqual({ value: 'I', message: '', result: true });
 });
 
-test("TC-2", function (assert) {
-    assert.propEqual(convertIntegerToRoman('X'), { value: 0, message: 'Please enter a valid integer', result: false }, "TC-2");
+test('TC-2', () => {
+  expect(convertIntegerToRoman('X')).toEqual({ value: 0, message: 'Please enter a valid integer', result: false });
 });
 
-test("TC-3", function (assert) {
-    assert.propEqual(convertRomanToInteger('X'), { value: 10, message: '', result: true }, "TC-3");
+test('TC-3', () => {
+  expect(convertRomanToInteger('X')).toEqual({ value: 10, message: '', result: true });
 });
 
-test("TC-4", function (assert) {
-    assert.propEqual(convertRomanToInteger('111'), { value: 0, message: 'Please enter a valid roman', result: false }, "TC-4");
+test('TC-4', () => {
+  expect(convertRomanToInteger('111')).toEqual({ value: 0, message: 'Please enter a valid roman', result: false });
 });
 
-test("TC-5", function (assert) {
-    assert.propEqual(convertIntegerToRoman(3999), { value: 'MMMCMXCIX', message: '', result: true }, "TC-5");
+test('TC-5', () => {
+  expect(convertIntegerToRoman(3999)).toEqual({ value: 'MMMCMXCIX', message: '', result: true });
 });
 
-test("TC-6", function (assert) {
-    assert.propEqual(convertIntegerToRoman(0), { value: 0, message: 'Out of range (1-3999)', result: false }, "TC-6");
+test('TC-6', () => {
+  expect(convertIntegerToRoman(0)).toEqual({ value: 0, message: 'Out of range (1-3999)', result: false });
 });
 
-test("TC-7", function (assert) {
-    assert.propEqual(convertIntegerToRoman(4000), { value: 0, message: 'Out of range (1-3999)', result: false }, "TC-7");
+test('TC-7', () => {
+  expect(convertIntegerToRoman(4000)).toEqual({ value: 0, message: 'Out of range (1-3999)', result: false });
 });
 
-test("TC-8", function (assert) {
-    assert.propEqual(convertRomanToInteger('IIII'), { value: 0, message: 'Please enter a valid roman', result: false }, "TC-8");
+test('TC-8', () => {
+  expect(convertRomanToInteger('IIII')).toEqual({ value: 0, message: 'Please enter a valid roman', result: false });
 });
 
-test("TC-9", function (assert) {
-    assert.propEqual(convertRomanToInteger(''), { value: 0, message: 'Please enter a valid roman', result: false }, "TC-9");
+test('TC-9', () => {
+  expect(convertRomanToInteger('')).toEqual({ value: 0, message: 'Please enter a valid roman', result: false });
 });
 
-test("TC-10", function (assert) {
-    assert.propEqual(convertRomanToInteger('MMMDCCCLXXXVIII'), { value: 3888, message: '', result: true }, "TC-10");
+test('TC-10', () => {
+  expect(convertRomanToInteger('MMMDCCCLXXXVIII')).toEqual({ value: 3888, message: '', result: true });
 });
 
-test("TC-11", function (assert) {
-    assert.propEqual(convertIntegerToRoman(58), { value: 'LVIII', message: '', result: true }, "TC-11");
+test('TC-11', () => {
+  expect(convertIntegerToRoman(58)).toEqual({ value: 'LVIII', message: '', result: true });
 });
 
-test("TC-12", function (assert) {
-    assert.propEqual(convertRomanToInteger('LVIII'), { value: 58, message: '', result: true }, "TC-12");
+test('TC-12', () => {
+  expect(convertRomanToInteger('LVIII')).toEqual({ value: 58, message: '', result: true });
 });
 
-test("TC-13", function (assert) {
-    assert.propEqual(convertIntegerToRoman(399), { value: 'CCCXCIX', message: '', result: true }, "TC-13");
+test('TC-13', () => {
+  expect(convertIntegerToRoman(399)).toEqual({ value: 'CCCXCIX', message: '', result: true });
 });
 
-test("TC-14", function (assert) {
-    assert.propEqual(convertRomanToInteger('CCCXCIX'), { value: 399, message: '', result: true }, "TC-14");
+test('TC-14', () => {
+  expect(convertRomanToInteger('CCCXCIX')).toEqual({ value: 399, message: '', result: true });
 });
